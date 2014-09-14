42b5d9e8bbe59e27402cf96f1f039016ce44149d
pablojgarea <pablojgarea@gmail.com>
2014-09-14 16:30:50 +0200

    Adaptación a web dicoruna.es

--- autonav/templates/tree/view.js
+++ autonav/templates/tree/view.js
@@ -1,6 +1,6 @@
-	$(document).ready(function () {
-    	$('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
-      	$('.tree .nodo > span').on('click', function (e) {
+  $(document).ready(function () {
+      $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
+        $('.tree .nodo > span').on('click', function (e) {
           var children = $(this).parent().parent().parent('li.parent_li').find('> ul > li');
           if (children.is(":visible")) {
               children.hide('fast');
@@ -10,21 +10,16 @@
               $(this).attr('title', 'Collapse this branch').find(' > i').addClass('fa-minus-circle').removeClass('fa-plus-circle');
           }
           e.stopPropagation();
-    	});
+      });
 
-      var url = window.location.pathname;
-      var padres= $('.tree a[href*="'+url+'"]').parentsUntil('.tree > .nivel1');
-      var nodo= $('.tree a[href*="'+url+'"]').parent().parent().parent().parent('li.parent_li');
-      var hijos = nodo.children('ul').children('li');
-      //nodo.children('> .carpeta, > .nodo').attr('id','actual');
+      var nodo= $('.tree li.nav-selected');
+      var padres= nodo.parentsUntil('.tree > .nivel1');
+      var hijos = nodo.find('> ul > li');
+
+      padres.attr('title', 'Collapse this branch').find('> .carpeta > .nodo > span > i, > .nodo > span > i').addClass('fa-minus-circle').removeClass('fa-plus-circle');
+      nodo.attr('title', 'Collapse this branch').find('> .carpeta > .nodo > span > i, > .nodo > span > i').addClass('fa-minus-circle').removeClass('fa-plus-circle');
       padres.show();
       nodo.show();
       hijos.show();
       
-      padres.attr('title', 'Collapse this branch').find('i').addClass('fa-minus-circle').removeClass('fa-plus-circle');      
-/*
-      hijos.attr('title', 'Collapse').find(' >  i').addClass('fa-plus-circle').removeClass('fa-minus-circle');
-      nodo.attr('title', 'Collapse').find(' > i').addClass('fa-minus-circle').removeClass('fa-plus-circle');
-
-*/
-  	});
+    });
